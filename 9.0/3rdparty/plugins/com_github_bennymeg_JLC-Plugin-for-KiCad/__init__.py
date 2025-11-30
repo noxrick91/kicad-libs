a106cb32fd956bc550f6e68f717380ba31e3ce22
Fake瑞克 <noxrick91@gmail.com>
2025-12-01 04:58:17 +0800

    清理目录

--- 9.0/3rdparty/plugins/com_github_bennymeg_JLC-Plugin-for-KiCad/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from .plugin import Plugin
-    plugin = Plugin()
-    plugin.register()
-except Exception as e:
-    import logging
-    logger = logging.getLogger()
-    logger.debug(repr(e))
--- 9.0/3rdparty/plugins/org_openscopeproject_InteractiveHtmlBom/web/user.js
+++
@@ -1,6 +0,0 @@
-// EventHandler.registerCallback(IBOM_EVENT_TYPES.BOM_BODY_CHANGE_EVENT, () => {
-//     for(var tr of bom.childNodes) {
-//         tr.onclick = tr.onmousemove;
-//         tr.onmousemove = null;
-//     };
-// });
